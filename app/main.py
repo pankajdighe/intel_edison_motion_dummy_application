fd608fa713e90fb0cc2a1dd733ac845362b82c8b
pankaj_dighe <pankajdighe77@gmail.com>
2016-12-02 00:17:09 -0800

    Added the changes

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import time
+import paho.mqtt.client as mqtt
+import random
+
+mqttc=mqtt.Client()
+mqttc.connect("iot.eclipse.org",1883,60)
+mqttc.loop_start()
+
+#read temperature
+def read_motion_data():
+    return random.randint(0, 500)
+
+#publish temperature
+while 1:
+    t=read_motion_data()
+    (result,mid)=mqttc.publish("topic/GeneralizedIoT/motion",t,2)
+    time.sleep(1)
+
+mqttc.loop_stop()
+mqttc.disconnect()
